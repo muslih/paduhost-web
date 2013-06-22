06f81e237c61d35f1f99d4d66490be1efcb9efa4
Muslih <muslihzarth@gmail.com>
2013-06-22 15:52:45 +0800

    setting up main.rb

---
+++ main.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'compass'
+
+get '/' do
+	erb :home
+end
+
+get '/kontak' do
+	erb :contact
+end
+
+not_found do
+    erb :not_found
+end
+
+get '/fake-error' do
+    status 500
+    "There's no wrong, really :p"
+end
